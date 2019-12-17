cefa4ebd6eb1b328f2b70cd184a642445160ae5a
jfalxa <jfalxa.sta@gmail.com>
2019-12-18 00:56:27 +0100

    display ledger lines for notes outside staff

--- src/components/score/notes.js
+++ src/components/score/notes.js
@@ -1,21 +1,28 @@
+import { Ledger } from './staff'
 import { Flat, Sharp } from './accidentals'
 
-export const WholeNote = ({ value = 3, tick = 0, accidental, alt = false }) => {
+export const WholeNote = ({ value = 0, tick = 0, accidental, alt = false }) => {
   const dx =
     WholeNote.offsetX +
-    tick * 2 * WholeNote.width +
+    tick * 2.5 * WholeNote.width +
     (alt ? WholeNote.width - 2 : 0)
 
-  const dy = 0.5 + (WholeNote.offsetY - value) * WholeNote.height
+  const dy = 0.5 + (WholeNote.offsetY - value) * (WholeNote.height / 2)
 
   return (
-    <g transform={`translate(${dx} ${dy})`}>
-      {accidental === 'b' && <Flat />}
-      {accidental === '#' && <Sharp />}
-      <path
-        d="m2.572214,5.620131c-1.383754,-0.425896 -2.47555,-1.583257 -2.47555,-2.624211c0,-2.946336 6.328451,-4.11228 8.941933,-1.647449c2.826166,2.665417 -1.786012,5.712197 -6.466382,4.27166zm4.221099,-0.740169c0.769934,-1.175067 0.033815,-3.503213 -1.299776,-4.110838c-1.958424,-0.892316 -3.158868,0.628287 -2.343195,2.968131c0.564183,1.618416 2.859518,2.338407 3.64297,1.142707z"
-        fill="#000000"
-      />
+    <g transform={`translate(${dx} 0)`}>
+      <g transform={`translate(0 ${dy})`}>
+        {accidental === 'b' && <Flat />}
+        {accidental === '#' && <Sharp />}
+        <path
+          d="m2.572214,5.620131c-1.383754,-0.425896 -2.47555,-1.583257 -2.47555,-2.624211c0,-2.946336 6.328451,-4.11228 8.941933,-1.647449c2.826166,2.665417 -1.786012,5.712197 -6.466382,4.27166zm4.221099,-0.740169c0.769934,-1.175067 0.033815,-3.503213 -1.299776,-4.110838c-1.958424,-0.892316 -3.158868,0.628287 -2.343195,2.968131c0.564183,1.618416 2.859518,2.338407 3.64297,1.142707z"
+          fill="#000000"
+        />
+      </g>
+
+      {Ledger.list(value).map(level => (
+        <Ledger key={level} level={level} />
+      ))}
     </g>
   )
 }
@@ -23,4 +30,4 @@ export const WholeNote = ({ value = 3, tick = 0, accidental, alt = false }) => {
 WholeNote.offsetX = 36
 WholeNote.offsetY = 9
 WholeNote.width = 10
-WholeNote.height = 3
+WholeNote.height = 6
--- src/components/score/staff.js
+++ src/components/score/staff.js
@@ -3,14 +3,14 @@ const Line = ({ level, ...props }) => {
 
   return (
     <line
-      {...props}
       x1="0"
-      y1={y}
       x2="100%"
+      y1={y}
       y2={y}
       stroke="black"
       strokeWidth="1px"
       shapeRendering="crispEdges"
+      {...props}
     />
   )
 }
@@ -18,11 +18,27 @@ const Line = ({ level, ...props }) => {
 Line.offset = 0.5
 Line.space = 6
 
+export const Ledger = props => {
+  return <Line x1="-1.5" x2="11.5" {...props} />
+}
+
+Ledger.list = value => {
+  if (value <= 0) {
+    const size = Math.floor(-value / 2) + 1
+    return [...Array(size).keys()].map(i => i + 5)
+  } else if (value >= 12) {
+    const size = Math.floor((value - 12) / 2) + 1
+    return [...Array(size).keys()].map(i => -(i + 1))
+  } else {
+    return []
+  }
+}
+
 export const Staff = ({ line = 0, children, ...props }) => {
   const dy = Staff.offset + line * Staff.size
 
   return (
-    <g {...props} transform={`translate(0 ${dy})`}>
+    <g transform={`translate(0 ${dy})`} {...props}>
       <Line level={0} />
       <Line level={1} />
       <Line level={2} />
