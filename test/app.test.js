4993e0090243731fc9314fc05cfcd90cfdea4443
jfalxa <jfalxa.sta@gmail.com>
2019-12-17 16:16:03 +0100

    dummy test file

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,3 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-
-import App from '../src/components/app'
-
-test('should render some text', () => {
-  const result = render(<App />)
-  const text = result.queryByText('HALLO')
-
-  expect(text.tagName).toBe('H1')
-  expect(text.innerHTML).toBe('HALLO')
+test('should be ok', () => {
+  expect(true).toBeTruthy()
 })
