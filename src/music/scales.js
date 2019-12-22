4c2eb9d2daea17517b42fb5401c4f0ed237f8574
jfalxa <jfalxa.sta@gmail.com>
2019-12-22 21:31:28 +0100

    simplify mode generation

--- src/music/scales.js
+++ src/music/scales.js
@@ -1,28 +1,10 @@
-export const MODES = [
-  '- Ionian\xA0\xA0\xA0\xA0\xA0(I)',
-  '- Dorian\xA0\xA0\xA0\xA0\xA0(II)',
-  '- Phrygian\xA0\xA0\xA0(III)',
-  '- Lydian\xA0\xA0\xA0\xA0\xA0(IV)',
-  '- Mixolydian\xA0(V)',
-  '- Aeolian\xA0\xA0\xA0\xA0(VI)',
-  '- Locrian\xA0\xA0\xA0\xA0(VII)'
-]
-
 const NOTES = [0, 1, 2, 3, 4, 5, 6]
-const MAJOR = [0, 2, 4, 5, 7, 9, 11]
+const MAJOR = [1, 1, 1 / 2, 1, 1, 1, 1 / 2]
 
-export const MODE_SCALES = NOTES.map(i => {
-  const left = MAJOR.slice(i)
-  const right = MAJOR.slice(0, i)
-
-  // prettier-ignore
-  return [
-    ...left.map(n => n - left[0]), 
-    ...right.map(n => n - left[0] + 12),
-    12
-  ]
-})
+export const MODES = NOTES.map(i => [...MAJOR.slice(i), ...MAJOR.slice(0, i)])
 
 export default function scale(root, mode) {
-  return MODE_SCALES[mode].map(degree => root + degree)
+  // stack the tone/half-tone progression successively on top of the root note
+  // and finally get the list of all the actual notes of the wanted scale
+  return MODES[mode].reduce((s, step, i) => [...s, s[i] + step * 2], [root])
 }
