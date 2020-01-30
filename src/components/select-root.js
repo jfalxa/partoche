0102afc0fc9311e613f50bb5ddbd28bec2a92892
jfalxa <jfalxa.sta@gmail.com>
2020-01-30 15:07:46 +0100

    show key note names instead of index in root selector

--- src/components/select-root.js
+++ src/components/select-root.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { number, func } from 'prop-types'
+import { keyToNote } from '../utils/conversion'
 
-const KEYS = Array(76)
-  .fill(0)
-  .map((_, i) => `- ${i}`)
+const KEYS = [...Array(76).keys()].map(keyToNote)
 
 const SelectRoot = ({ root, setRoot, ...props }) => (
   <select
@@ -13,9 +12,9 @@ const SelectRoot = ({ root, setRoot, ...props }) => (
     css={{ fontFamily: 'monospace' }}
   >
     <option value={-1}>- No root</option>
-    {KEYS.map((root, i) => (
-      <option key={root} value={i}>
-        {root}
+    {KEYS.map(root => (
+      <option key={root.key} value={root.key}>
+        - {`${root.note}${root.octave}`}
       </option>
     ))}
   </select>
